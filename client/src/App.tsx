705033fd5ba83e23c4f14e1f7b169e954e1984fc
Andrew Lee <andrewzhlee@gmail.com>
2024-04-20 18:55:34 -0400

    clean template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {}
+
+const App: React.FC<IProps> = (props) => {
+    return (
+        <>
+            <CssBaseline />
+        </>
+      );
 }
 
 export default App;
