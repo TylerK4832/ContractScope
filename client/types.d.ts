bbf743331841a816017081f6f1459346765081b8
Tim Vuong <timvuong13@gmail.com>
2024-04-20 19:21:44 -0400

    Type

---
+++ client/types.d.ts
@@ -0,0 +1,15 @@
+export interface Contract {
+  context: string;
+  date: Date;
+  awardedCompany: string;
+  awardedCompanyLocation: string;
+  amountAwarded: number;
+  awardedFor: string;
+  workLocation: string;
+  expectedCompletionDate: Date;
+  contractingActivityGroup: string;
+  activityGroupLocation: string;
+  activityCode: string;
+  contractType: string;
+  isModification: boolean;
+}
