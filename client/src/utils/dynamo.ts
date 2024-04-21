c5b3b3225c1d23d921880ccd5396f312474bb34c
beelauuu <blau1@terpmail.umd.edu>
2024-04-21 08:08:08 -0400

    pagination

--- client/src/utils/dynamo.ts
+++ client/src/utils/dynamo.ts
@@ -12,68 +12,71 @@ AWS.config.update({
 const dynamodb = new AWS.DynamoDB();
 
 const params = {
-    TableName: 'FinalContracts',
-    // Add any conditions or filters here if needed
-  };
+  TableName: 'FinalContracts',
+  // Add any conditions or filters here if needed
+};
 
 export const getDataFromDB = async (): Promise<Contract[]> => {
-    return new Promise((resolve, reject) => {
-        dynamodb.scan(params, (err, data) => {
-            if (err) {
-                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-                reject(err);
-            } else {
-                const processed: Contract[] = processData(data);
-                resolve(processed);
-            }
-        });
-    });
+  let allContracts: Contract[] = [];
+  let lastEvaluatedKey: AWS.DynamoDB.Key | undefined = undefined;
+
+  do {
+    const result: AWS.DynamoDB.DocumentClient.ScanOutput = await scanWithPagination(params, lastEvaluatedKey);
+    const processed = processData(result.Items);
+    allContracts = allContracts.concat(processed);
+    lastEvaluatedKey = result.LastEvaluatedKey;
+  } while (lastEvaluatedKey);
+
+  return allContracts;
 }
 
-const processData= (data: AWS.DynamoDB.ScanOutput): Contract[] => {
-    const contracts: Contract[] = [];
+const scanWithPagination = async (params: AWS.DynamoDB.DocumentClient.ScanInput, lastEvaluatedKey?: AWS.DynamoDB.Key): Promise<AWS.DynamoDB.DocumentClient.ScanOutput> => {
+  if (lastEvaluatedKey) {
+    params.ExclusiveStartKey = lastEvaluatedKey;
+  }
 
-    if (data.Items === undefined) return contracts;
-
-    let count = 0;
-    for (let item of data.Items) {
-
-        const results = cleaner(item.contracts.L as RawContract[]);
-        contracts.push(...results);
-        if (count++ === 20) break;
-    }
-
-    return contracts;
+  return dynamodb.scan(params).promise();
 }
 
-const cleaner = (rawRows: RawContract[]): Contract[] => { 
-    return rawRows.map((r) => ({
-            id: Math.random().toFixed(10).toString(),
-            workLocation: r.M.workLocation.S,
-            amountAwarded: r.M.amountAwarded.S,
-            contractingActivity: r.M.contractingActivity.S,
-            awardedFor: r.M.awardedFor.S,
-            contractType: r.M.contractType.S,
-            companyName: r.M.companyName.S,
-            contractNumber: r.M.contractNumber.S,
-            completionDate: formatDate(r.M.completionDate.S),
-        }));
+const processData = (items?: AWS.DynamoDB.DocumentClient.ItemList): Contract[] => {
+  const contracts: Contract[] = [];
+
+  if (!items) return contracts;
+
+  for (const item of items) {
+    const results = cleaner(item.contracts.L as RawContract[]);
+    contracts.push(...results);
+  }
+
+  return contracts;
+}
+
+const cleaner = (rawRows: RawContract[]): Contract[] => {
+  return rawRows.map((r) => ({
+    id: Math.random().toFixed(10).toString(),
+    workLocation: r.M.workLocation.S,
+    amountAwarded: r.M.amountAwarded.S,
+    contractingActivity: r.M.contractingActivity.S,
+    awardedFor: r.M.awardedFor.S,
+    contractType: r.M.contractType.S,
+    companyName: r.M.companyName.S,
+    contractNumber: r.M.contractNumber.S,
+    completionDate: formatDate(r.M.completionDate.S),
+  }));
 }
 
 const formatDate = (datestr: string): string => {
-    datestr = datestr.replace(/fiscal/ig, 'Jan.');
+  datestr = datestr.replace(/fiscal/ig, 'Jan.');
 
-    const parts = datestr.split(' ');
+  const parts = datestr.split(' ');
 
-    if (parts.length === 1) {
-        return `Jan. 1 ${parts[0]}`;
-    }
-    else if (parts.length === 2) {
-        return `${parts[0]} 1, ${parts[1]}`
-    }
-    else if (parts.length > 3) {
-        return `${parts[0]} ${parts[1]}, ${parts[2]}`;
-    }
+  if (parts.length === 1) {
+    return `Jan. 1 ${parts[0]}`;
+  } else if (parts.length === 2) {
+    return `${parts[0]} 1, ${parts[1]}`
+  } else if (parts.length > 3) {
+    return `${parts[0]} ${parts[1]}, ${parts[2]}`;
+  }
 
-    return datestr;
+  return datestr;
 }
