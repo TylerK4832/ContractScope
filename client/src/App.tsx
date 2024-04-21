2e84f5a11385c49954aff39da096d4d523b9a7ab
Tim Vuong <timvuong13@gmail.com>
2024-04-20 20:08:39 -0400

    Mock data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import './App.css';
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import BasicExampleDataGrid from "./components/table";
 
 interface IProps {}
 
 const App: React.FC<IProps> = (props) => {
-    return (
-        <>
-            <CssBaseline />
-            {/* Add project code beneath the baseline */}
-        </>
-      );
-}
+  return (
+    <>
+      <CssBaseline />
+      <BasicExampleDataGrid />
+      {/* Add project code beneath the baseline */}
+    </>
+  );
+};
 
 export default App;
